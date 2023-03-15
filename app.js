eefc43acddbdf8af1c515beee47ee22dc2558409
shubham kumar <clboy768@gmail.com>
2023-03-15 19:36:35 +0530

    Creating Nodes and Modifying Dom added

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
 
-// var items= document.getElementsByClassName('list-group-item')
-// console.log(items)
-// console.log(items[2]);
 
-// items[2].style.backgroundColor="green";
-// for(let i=0 ; i<items.length; i++){
-//     items[i].style.fontWeight="bold"
-// }
-
-
-// var new_li =document.getElementsByClassName("new-list-item");
-//     new_li[0].style.color="red";
-//     console.log(new_li)
-
-//     var li= document.getElementsByName("li-5");
-//     li[0].style.backgroundColor="pink"
-
-//Using QuerySelectorALL change the font color to green for 2nd item in the item list
-// var seconditem=document.querySelector('.list-group-item:nth-child(2)');
-// seconditem.style.backgroundColor="green"
-// var thirditem=document.querySelector('.list-group-item:nth-child(3)');
-// thirditem.style.display="none"
-
-// query selector all
-
-var seconditem=document.querySelectorAll('li:nth-child(n)');
-var odd= document.querySelectorAll('li:nth-child(odd)')
-for(var i=0;i<seconditem.length; i++){
-    if(i==1){
-        seconditem[i].style.color="green"
-    }
-}
-for(var i=0;i<odd.length; i++){
-    odd[i].style.backgroundColor="green";
-}
-
+let newDiv = document.createElement('div')
+newDiv.className = 'hello'
+newDiv.id = 'hello1'
+newDiv.setAttribute('title', 'hello Div')
+var newDivText = document.createTextNode('Hello Word')
+// append text to div
+newDiv.appendChild(newDivText);
+var container= document.querySelector('header .container')
+var h1=document.querySelector('header h1');
+console.log(newDiv)
+container.insertBefore(newDiv,h1)
+let container1=document.getElementsByClassName('list-group')[0]
+let li=document.getElementsByClassName('list-group-item')
+console.log(li[0]);
+container1.insertBefore(newDivText,li[0])
