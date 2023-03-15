cc646b68ea67830d8b43cf6c653b4e9eb27d5d68
shubham kumar <clboy768@gmail.com>
2023-03-15 15:57:10 +0530

    adding new li

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 
-var items= document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[2]);
+// var items= document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[2]);
 
-items[2].style.backgroundColor="green";
-for(let i=0 ; i<items.length; i++){
-    items[i].style.fontWeight="bold"
-}
+// items[2].style.backgroundColor="green";
+// for(let i=0 ; i<items.length; i++){
+//     items[i].style.fontWeight="bold"
+// }
+
+
+var new_li =document.getElementsByClassName("new-list-item");
+    new_li[0].style.color="red";
+
+    var li= document.getElementsByName("li-5");
+    li[0].style.backgroundColor="pink"
